b55851eb9bcd7bbf24e1a34156334fcddde9deee
Paloma-Cruz <58375859+Paloma-Cruz@users.noreply.github.com>
2020-01-11 16:24:43 -0300

    Create Media2.py

---
+++ Media2.py
@@ -0,0 +1,8 @@
+A = float(input())
+B = float(input())
+C = float(input())
+Alinha = A * 2
+Blinha = B * 3
+Clinha = C * 5
+MEDIA = (Alinha + Blinha + Clinha) / (2 + 3 + 5)
+print('MEDIA = %.1f' %MEDIA)
