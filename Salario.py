98a948dd907f974194340cdcb8799db62d0667b7
Paloma-Cruz <58375859+Paloma-Cruz@users.noreply.github.com>
2020-01-11 16:26:14 -0300

    Create Salario.py

---
+++ Salario.py
@@ -0,0 +1,6 @@
+numero_funcionario = int(input())
+horas_trabalhadas = float(input())
+salario_hora = float(input())
+salario_final = horas_trabalhadas * salario_hora
+print('NUMBER = ' + str(numero_funcionario))
+print('SALARY = U$ %.2f' %salario_final)
